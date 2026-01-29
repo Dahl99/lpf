58b3ff07da6b89c97cebeb69582e89dbaf90a1c0
Dahl99 <danieldahl99@gmail.com>
2026-01-29 21:34:47 +0100

    Simplify lowpass filter

--- LowpassFilter.cpp
+++ LowpassFilter.cpp
@@ -1,37 +1,16 @@
-#include <cmath>
-
 #include "LowpassFilter.h"
 
-LowpassFilter::LowpassFilter(const double cutoffFrequency, const double resistance, const double capacitance)
+#include <cmath>
+
+LowpassFilter::LowpassFilter(const double cutoffFrequency)
 		: m_cutoffFrequency(cutoffFrequency)
-		, m_resistance(resistance)
-		, m_capacitance(capacitance)
 		, m_output(0.0)
 {}
 
-double LowpassFilter::calculateResistance() const
+double LowpassFilter::recalculateOutput(const double input, const double deltaTime)
 {
-	return 1 / (2 * M_PI * m_cutoffFrequency * m_capacitance);
-}
-
-double LowpassFilter::calculateCapacitance() const
-{
-	return 1 / (2 * M_PI * m_cutoffFrequency * m_resistance);
-}
-
-double LowpassFilter::calculateCutoffFrequency() const
-{
-	return 1 / (2 * M_PI * m_resistance * m_capacitance);
-}
-
-double LowpassFilter::calculateTransfer(const double dt) const
-{
-	return 1 / (1 + dt * m_resistance * m_capacitance);
-}
-
-double LowpassFilter::update(const double input, const double deltaTime)
-{
-	m_output += (input - m_output) * (1-exp(-deltaTime * 2 * M_PI * m_cutoffFrequency));
+	// Calculates the voltage output using a first-order lowpass filter formula
+	m_output += (1-exp(-2 * M_PI * m_cutoffFrequency * deltaTime)) * (input - m_output);
 
 	return m_output;
 }
--- LowpassFilter.h
+++ LowpassFilter.h
@@ -5,21 +5,9 @@ class LowpassFilter
 private:
 		double m_cutoffFrequency;
 private:
-		double m_resistance;
-private:
-		double m_capacitance;
-private:
-		mutable double m_output;
+		double m_output;
 public:
-		LowpassFilter(const double cutoffFrequency, const double resistance, const double capacitance);
+		explicit LowpassFilter(const double cutoffFrequency);
 public:
-		[[nodiscard]] double calculateResistance() const;
-public:
-		[[nodiscard]] double calculateCapacitance() const;
-public:
-		[[nodiscard]] double calculateCutoffFrequency() const;
-public:
-		[[nodiscard]] double calculateTransfer(const double dt) const;
-public:
-		double update(const double input, const double deltaTime);
+		double recalculateOutput(const double input, const double deltaTime);
 };
