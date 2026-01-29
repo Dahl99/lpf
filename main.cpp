a877ea2584a83f78ba320c3c3d6ac48a7f526e95
Dahl99 <danieldahl99@gmail.com>
2026-01-29 22:54:31 +0100

    Expand test

--- main.cpp
+++ main.cpp
@@ -26,16 +26,24 @@ void TestLowpassFilterCorrectness()
     constexpr double cutoffFrequency = 1.0; // Hz
     constexpr double input = 1.0; // Volts
     constexpr double deltaTime = 0.001; // seconds
+    constexpr double tolerance = 1e-9;
+
     Core::LowpassFilter lpf(cutoffFrequency);
 
     // Calculate expected output using the filter formula
-    constexpr double expected = 0.006263487f;
-    constexpr double tolerance = 1e-9;
-    const double output = lpf.RecalculateOutput(input, deltaTime);
-
-    if (std::abs(output - expected) < tolerance) {
-        std::cout << "LowpassFilter test passed! Output: " << output << ", Expected: " << expected << std::endl;
+    constexpr double expectedOneMs = 0.006263487f;
+    double output = lpf.RecalculateOutput(input, deltaTime);
+    if (std::abs(output - expectedOneMs) < tolerance) {
+        std::cout << "LowpassFilter test 1ms passed! Output: " << output << ", Expected: " << expectedOneMs << std::endl;
     } else {
-        std::cout << "LowpassFilter test FAILED! Output: " << output << ", Expected: " << expected << std::endl;
+        std::cout << "LowpassFilter test 1ms FAILED! Output: " << output << ", Expected: " << expectedOneMs << std::endl;
+    }
+
+    constexpr double expectedTwoMs = 0.012487743f;
+    output = lpf.RecalculateOutput(input, deltaTime);
+    if (std::abs(output - expectedTwoMs) < tolerance) {
+        std::cout << "LowpassFilter test 2ms passed! Output: " << output << ", Expected: " << expectedTwoMs << std::endl;
+    } else {
+        std::cout << "LowpassFilter test 2ms FAILED! Output: " << output << ", Expected: " << expectedTwoMs << std::endl;
     }
 }
